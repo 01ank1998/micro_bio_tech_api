95f502d3a206a0e6c5e45347049dbe964ba113b9
VishruthBharadw <01ankith1998@gmail.com>
2020-06-29 16:01:41 +0530

    update

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-/**
- * @type Application Bootstrap
- * @desc API server
- */
-
-/* Import all the required libraries */
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const helmet = require("helmet");
 const config = require("config");
 const glob = require("glob");
 const path = require("path");
@@ -19,13 +12,10 @@ const authenticator = require("./controllers/auth");
 const logger = require("./lib/utils/logger");
 const db = require("./lib/utils/db");
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 
-// parse application/json
 app.use(bodyParser.json({ limit: "50mb" }));
 
-/* Check CORS before proceeding further */
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -51,19 +41,10 @@ app.use(
   })
 );
 
-/* Add additional http flags in response header for security */
-app.use(helmet());
+app.use(authenticator._fakeAuth);
 
-/* Add interceptor middleware to catch all responses sent out */
-//app.use(interceptor);
-
-/* Add auth middleware */
-app.use(authenticator._fakeAuth); // use _fakeAuth when testing
-
-/* Allow reverse proxy such as NginX, AWS ELB */
 app.enable("trust proxy");
 
-/* Apply rate limit to all requests - 429 code will be sent */
 const limiter = rateLimit({
   windowMs: config.get("rateLimit.minutes") * 60 * 1000, // duration in minutes
   max: config.get("rateLimit.maxRequests"), // limit each IP to n (config.maxRequests) requests per windowMs
@@ -76,7 +57,6 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-/* Recursively include all routes inside ./routes */
 function loadRoutes() {
   glob.sync("./routes/**/*.js").forEach(function (file) {
     app.use("/" + config.get("api.version") + "/", require(path.resolve(file)));
@@ -90,7 +70,6 @@ function loadModels() {
   });
 }
 
-/* Default response for 404 errors */
 function handle404Error() {
   app.use(function (req, res) {
     res.status(404).json({
@@ -99,7 +78,7 @@ function handle404Error() {
     });
   });
 }
-/* Start API server on the specified port */
+
 function startServer() {
   http.listen(config.get("api.port"));
   logger.info({ description: `server started on ${config.get("api.port")}` });
